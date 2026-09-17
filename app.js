b6db0f601b4347cc4c08a7ff87dd4dabf13dcdd3
alfieawatv <alfie.awatv@gmail.com>
2026-09-18 00:21:02 +0100

    Restore stable app (UK) while worldwide OSM ships via upload

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-(function(){
-  function fail(e){ console.error(e); document.body.innerHTML='<div style="padding:2rem;font-family:system-ui;text-align:center"><h1>Could not load app</h1><p>Refresh please.</p></div>'; }
-  var n=22;
-  var files=[];
-  for(var i=0;i<n;i++) files.push('./b'+i+'.txt?v=1');
-  Promise.all(files.map(function(u){
-    return fetch(u).then(function(r){ if(!r.ok) throw new Error(u); return r.text(); });
-  })).then(function(parts){
-    var code = atob(parts.join(''));
-    (0,eval)(code);
-  }).catch(fail);
+(function () {
+  var s = document.createElement('script');
+  s.src = 'https://cdn.jsdelivr.net/gh/alfieawatv/gottogo-uk@acdf92859e43e5e4c0efb50b24f9b66ee884c43c/app.js';
+  s.onerror = function () {
+    var s2 = document.createElement('script');
+    s2.src = 'https://raw.githubusercontent.com/alfieawatv/gottogo-uk/acdf92859e43e5e4c0efb50b24f9b66ee884c43c/app.js';
+    s2.onerror = function () {
+      document.body.innerHTML = '<div style="padding:2rem;font-family:system-ui;text-align:center"><h1>Could not load app</h1><p>Please hard-refresh or try again shortly.</p></div>';
+    };
+    document.head.appendChild(s2);
+  };
+  document.head.appendChild(s);
 })();
