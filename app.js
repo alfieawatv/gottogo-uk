604bf1f8b2d5a586b919ce113c56da81844768f5
alfieawatv <alfie.awatv@gmail.com>
2026-09-18 00:17:39 +0100

    Loader for 10-part worldwide OSM app

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
     console.error(e);
     document.body.innerHTML='<div style="padding:2rem;font-family:system-ui;text-align:center"><h1>Could not load app</h1><p>Please refresh.</p></div>';
   }
-  var n=6;
+  var n=10;
   var files=[];
-  for(var i=0;i<n;i++) files.push('./app.part'+i+'.js?v=ww1');
+  for(var i=0;i<n;i++) files.push('./app.part'+i+'.js?v=ww2');
   Promise.all(files.map(function(u){
     return fetch(u).then(function(r){ if(!r.ok) throw new Error(u+' '+r.status); return r.text(); });
   })).then(function(parts){ (0,eval)(parts.join('')); }).catch(fail);
