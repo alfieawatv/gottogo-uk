dc410a6e747062bb8997701eef0b49b0f69b5428
alfieawatv <alfie.awatv@gmail.com>
2026-09-17 20:39:35 +0100

    Bump cache to v6 after data loading fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gottogo-v5';
+const CACHE = 'gottogo-v6';
 const ASSETS = ['./', './index.html', './app.js', './styles.css', './manifest.json', './icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
