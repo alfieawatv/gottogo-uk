a17cc4454e9dda6f2f045b3822d0baa48b1342e2
alfieawatv <alfie.awatv@gmail.com>
2026-09-19 00:19:51 +0100

    Load 4-part instant app (cache + progressive + Overpass race)

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
     console.error(e);
     document.body.innerHTML = '<div style="padding:2rem;font-family:system-ui;text-align:center"><h1>Could not load app</h1><p>Please refresh.</p></div>';
   }
-  var n = 19;
+  var n = 4;
   var files = [];
-  for (var i = 0; i < n; i++) files.push('./p' + i + '.js?v=w5');
+  for (var i = 0; i < n; i++) files.push('./f' + i + '.js?v=s2');
   Promise.all(files.map(function (u) {
     return fetch(u).then(function (r) {
       if (!r.ok) throw new Error(u + ' ' + r.status);
