2eaf4a98d46ca362e992ae91c8e1190bb27d594c
alfieawatv <alfie.awatv@gmail.com>
2026-09-18 06:45:17 +0100

    ww p11

---
+++ p11.js
@@ -0,0 +1,20 @@
+      '<div class="stat"><div class="stat-l">Distance</div><div class="stat-v">' + formatDist(dd) + '</div></div>' +
+      '<div class="stat"><div class="stat-l">Source</div><div class="stat-v">' + (t.src === 'osm' ? 'OpenStreetMap' : 'Toilet Map') + '</div></div>' +
+      '</div>' +
+      (t.hours ? '<div class="note">Today: ' + esc(t.hours) + '</div>' : '') +
+      (t.notes ? '<div class="note">' + esc(t.notes) + '</div>' : '') +
+      '<div class="actions">' + report + '</div>' +
+      '<p class="credit">Created by Alfie Watts \u00b7 Data \u00a9 OpenStreetMap contributors' +
+      (t.src === 'toiletmap' ? ' + Toilet Map (CC BY 4.0)' : '') + '</p>';
+    d.classList.add('open');
+    $('detailBack').onclick = function () { d.classList.remove('open'); state.selected = null; applyList(); };
+    $('favBtn').onclick = function () {
+      if (state.favs[t.i]) delete state.favs[t.i];
+      else state.favs[t.i] = 1;
+      saveJson(LS_FAV, state.favs);
+      showDetail(t);
+      applyList();
+    };
+  }
+
+  /* ---------- Search (Nominatim worldwide) ---------- */
